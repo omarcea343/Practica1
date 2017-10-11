c323012c7c6b199d0a227c34b917268adf3fa5cf
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-10-11 13:36:13 -0500

    final
    El algoritmos final ya funcionando

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include<stdio.h> 
+int main (){
+	
+	int aux,i,j,k,n=20; 
+	int ordenar[20]; 
+	
+	for (i=0; i<n; i++){ 
+		printf("Ingresa el %d \n",i+1); 
+		scanf("%d",&ordenar[i]); 
+	} 
+	for (i=0;i<n;i++){ 
+		for (j=0;j<n-i;j++){ 
+			if (ordenar[j]>=ordenar[j+1]){ 
+				aux=ordenar[j]; 
+				ordenar[j]=ordenar[j+1]; 
+				ordenar[j+1]=aux; 
+			} 
+		} 
+	} 
+	for (k=0;k<n;k++){ 
+		printf(" %d",ordenar[k]); 
+	} 
 
-int main(){
-	
-	
-	return 0;
-}
+	return 0; 
+} 
